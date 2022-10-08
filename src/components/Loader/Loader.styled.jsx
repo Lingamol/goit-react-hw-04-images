3910212a12a46c56ed27a80485c0c95b7e434fde
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-10-09 00:25:20 +0300

    Update Loader.styled.jsx

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,7 +1,14 @@
 import { ThreeCircles } from 'react-loader-spinner';
 import styled from '@emotion/styled';
 export const SpinerWraper = styled.div`
-  margin-right: auto;
-  margin-left: auto;
+  position: fixed;
+  /* bottom: 20px; */
+
+  z-index: 99;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  /* right: 30px; */
 `;
 export const Spiner = styled(ThreeCircles)``;
