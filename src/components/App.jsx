5e919e2e97145b8c90d96547306c73692fbb6887
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-10-13 23:18:28 +0300

    hook start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,60 +6,49 @@ import ImageGallery from './ImageGallery';
 import Loader from './Loader';
 import Modal from './Modal';
 import SearchBar from './Searchbar';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { AppWrapper } from './App.styled';
 // import { hits } from '../js/data';
 import { fetchImagesWithQuery, PER_PAGE } from 'services/api';
 import GalleryPagination from 'components/GalleryPagination';
 // import GallaryContentLoader from 'GallaryContentLoader';
 
-export class App extends Component {
-  state = {
-    // showModal: false,
-    isLoading: false,
-    error: null,
-    galleryColection: [],
-    activeGalleryItem: null,
-    search: '',
-    page: 1,
-    totalHits: 0,
-    pagination: 'LoadMore',
-  };
-  componentDidMount() {
-    // console.log('App Mount');
-  }
+export const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [galleryColection, setGalleryColection] = useState([]);
+  const [activeGalleryItem, setActiveGalleryItem] = useState(null);
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [pagination, setPagination] = useState('LoadMore');
 
-  componentWillUnmount() {
-    // console.log('App WillUnmount');
-  }
+  //  const componentDidUpdate(prevProps, prevState, snapshot) {
+  //     // console.log('App componentDidUpdate');
+  //     const { page, search, pagination } = this.state;
+  //     if (
+  //       prevState.search !== search ||
+  //       prevState.page !== page ||
+  //       prevState.pagination !== pagination
+  //     ) {
+  //       this.FetchImg();
+  //    }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    // console.log('App componentDidUpdate');
-    const { page, search, pagination } = this.state;
-    if (
-      prevState.search !== search ||
-      prevState.page !== page ||
-      prevState.pagination !== pagination
-    ) {
-      this.FetchImg();
-    }
-    // if (prevState.pagination !== pagination && search !== '') {
-    //   this.setState({ galleryColection: [], page: 1 });
-    //   this.FetchImg();
-    // }
-  }
+  //     // if (prevState.pagination !== pagination && search !== '') {
+  //     //   this.setState({ galleryColection: [], page: 1 });
+  //     //   this.FetchImg();
+  //     // }
+  //   }
+  // useEffect(() => async () => FetchImg(), [search, page, pagination]);
 
-  FetchImg = async () => {
-    const { page, pagination } = this.state;
-    if (this.state.search === '') {
+  const FetchImg = async () => {
+    // const { page, pagination } = this.state;
+    if (search === '') {
       return;
     }
-    this.setState({ isLoading: true });
+    setIsLoading(true);
     try {
-      const data = await fetchImagesWithQuery(
-        this.state.search,
-        this.state.page
-      );
+      const data = await fetchImagesWithQuery(search, page);
       const { hits, totalHits } = data;
       if (hits.length === 0) {
         toast.warn(
@@ -76,20 +65,25 @@ export class App extends Component {
         );
         return;
       } else if (page === 1 || pagination === 'Pagination') {
-        this.setState({
-          galleryColection: [...hits],
-          totalHits,
-        });
+        setGalleryColection([...hits]);
+        setTotalHits(totalHits);
+        // this.setState({
+        //   galleryColection: [...hits],
+        //   totalHits,
+        // });
       } else if (pagination === 'LoadMore') {
-        this.setState(state => ({
-          galleryColection: [...state.galleryColection, ...hits],
-          totalHits,
-        }));
+        // this.setState(state => ({
+        //   galleryColection: [...state.galleryColection, ...hits],
+        //   totalHits,
+        // }));
+        setGalleryColection(prevState => [...prevState, ...hits]);
+        setTotalHits(totalHits);
       }
     } catch (error) {
-      this.setState({
-        error,
-      });
+      // this.setState({
+      //   error,
+      // });
+      setError(error);
       toast.error('Sorry, something going wrong :(', {
         position: 'top-right',
         autoClose: 5000,
@@ -100,58 +94,67 @@ export class App extends Component {
         progress: undefined,
       });
     } finally {
-      this.setState({ isLoading: false });
+      // this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  toggleModal = () => {
+  const toggleModal = () => {
     // this.setState(({ showModal }) => ({
     //   showModal: !showModal,
     // }));
-    this.setState(({ activeGalleryItem }) => ({
-      activeGalleryItem: null,
-    }));
+    setActiveGalleryItem(null);
+    // this.setState(({ activeGalleryItem }) => ({
+    //   activeGalleryItem: null,
+    // }));
   };
-  radioBtnChange = value => {
+  const radioBtnChange = value => {
     // this.setState(({ showModal }) => ({
     //   showModal: !showModal,
     // }));
     console.log(value);
-    this.setState({ pagination: value, page: 1, galleryColection: [] });
+    setPagination(value);
+    setPage(1);
+    setGalleryColection([]);
+    // this.setState({ pagination: value, page: 1, galleryColection: [] });
   };
 
-  onSelectGalleryItem = item => {
-    this.setState({ activeGalleryItem: item });
+  const onSelectGalleryItem = item => {
+    // this.setState({ activeGalleryItem: item });
+    setActiveGalleryItem(item);
   };
 
-  heandleSubmitForm = ({ search }) => {
+  const heandleSubmitForm = ({ search: searchForm }) => {
     // console.log(pagination);
     // if (pagination !== 'LoadMore') {
     //   this.setState({ pagination: true });
     // } else if (this.state.pagination) {
     //   this.setState({ pagination: false });
     // }
-    const normalizedSearch = search.toLocaleLowerCase();
-    if (normalizedSearch && normalizedSearch !== this.state.search) {
-      this.setState({
-        search: normalizedSearch,
-        page: 1,
-        galleryColection: [],
-      });
+    const normalizedSearch = searchForm.toLocaleLowerCase();
+    if (normalizedSearch !== search) {
+      // this.setState({
+      //   search: normalizedSearch,
+      //   page: 1,
+      //   galleryColection: [],
+      // });
+      console.log(normalizedSearch);
+      setSearch(normalizedSearch);
+      setPage(1);
+      setGalleryColection([]);
     }
   };
-  OnClickLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const OnClickLoadMore = () => {
+    setPage(prevState => prevState + 1);
   };
-  OnPagination = page => {
+  const OnPagination = page => {
     // console.log(page);
-    this.setState({ page });
+    // this.setState({ page });
+    setPage(page);
   };
 
-  onDisableLoadMore = () => {
-    const { totalHits, page } = this.state;
+  const onDisableLoadMore = () => {
+    // const { totalHits, page } = this.state;
     if (!totalHits) {
       return true;
     } else if (totalHits - PER_PAGE * page < 0) {
@@ -161,16 +164,16 @@ export class App extends Component {
     }
   };
 
-  showBtnLoadMore = () => {
-    const { totalHits } = this.state;
+  const showBtnLoadMore = () => {
+    // const { totalHits } = this.state;
     if (totalHits / PER_PAGE > 1) {
       return true;
     } else {
       return false;
     }
   };
-  showGallery = () => {
-    const { totalHits } = this.state;
+  const showGallery = () => {
+    // const { totalHits } = this.state;
     if (totalHits) {
       return true;
     } else {
@@ -178,64 +181,285 @@ export class App extends Component {
     }
   };
 
-  render() {
-    const { activeGalleryItem, isLoading, totalHits, pagination } = this.state;
-    // console.log(this.state.gellaryColection);
-    const showBtnLoadMore = this.showBtnLoadMore();
-    const showGallery = this.showGallery();
-    const countPages = Math.ceil(totalHits / PER_PAGE);
-    return (
-      <AppWrapper>
-        <SearchBar
-          onSubmit={this.heandleSubmitForm}
-          paginationMode={pagination}
-          isSubmitting={isLoading}
-          radioBtnChange={this.radioBtnChange}
+  // console.log(this.state.gellaryColection);
+  // const showBtnLoadMore = showBtnLoadMore();
+  // const showGallery = showGallery();
+  const countPages = Math.ceil(totalHits / PER_PAGE);
+  return (
+    <AppWrapper>
+      <SearchBar
+        onSubmit={heandleSubmitForm}
+        paginationMode={pagination}
+        isSubmitting={isLoading}
+        radioBtnChange={radioBtnChange}
+      />
+      {/* {isLoading && galleryColection.length === 0 && <GallaryContentLoader />} */}
+      {isLoading && <Loader />}
+      {showGallery() && (
+        <ImageGallery
+          galleryColection={galleryColection}
+          onSelectGalleryItem={item => {
+            onSelectGalleryItem(item);
+          }}
         />
-        {/* {isLoading && galleryColection.length === 0 && <GallaryContentLoader />} */}
-        {isLoading && <Loader />}
-        {showGallery && (
-          <ImageGallery
-            galleryColection={this.state.galleryColection}
-            onSelectGalleryItem={item => {
-              this.onSelectGalleryItem(item);
-            }}
-          />
-        )}
+      )}
 
-        {activeGalleryItem && (
-          <Modal
-            onClose={this.toggleModal}
-            activeGalleryItem={this.state.activeGalleryItem}
-          />
-        )}
+      {activeGalleryItem && (
+        <Modal onClose={toggleModal} activeGalleryItem={activeGalleryItem} />
+      )}
 
-        {showBtnLoadMore && pagination === 'LoadMore' && (
-          <Button
-            onLoadMore={this.OnClickLoadMore}
-            onDisableLoadMore={this.onDisableLoadMore}
-          />
-        )}
-        {pagination === 'Pagination' && totalHits / PER_PAGE > 1 && (
-          <GalleryPagination
-            onPagination={this.OnPagination}
-            countPages={countPages}
-            currentPage={this.state.page}
-          />
-        )}
-
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
+      {showBtnLoadMore() && pagination === 'LoadMore' && (
+        <Button
+          onLoadMore={OnClickLoadMore}
+          onDisableLoadMore={onDisableLoadMore}
         />
-      </AppWrapper>
-    );
-  }
-}
+      )}
+      {pagination === 'Pagination' && totalHits / PER_PAGE > 1 && (
+        <GalleryPagination
+          onPagination={OnPagination}
+          countPages={countPages}
+          currentPage={page}
+        />
+      )}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </AppWrapper>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     // showModal: false,
+//     isLoading: false,
+//     error: null,
+//     galleryColection: [],
+//     activeGalleryItem: null,
+//     search: '',
+//     page: 1,
+//     totalHits: 0,
+//     pagination: 'LoadMore',
+//   };
+//   componentDidMount() {
+//     // console.log('App Mount');
+//   }
+
+//   componentWillUnmount() {
+//     // console.log('App WillUnmount');
+//   }
+
+//   componentDidUpdate(prevProps, prevState, snapshot) {
+//     // console.log('App componentDidUpdate');
+//     const { page, search, pagination } = this.state;
+//     if (
+//       prevState.search !== search ||
+//       prevState.page !== page ||
+//       prevState.pagination !== pagination
+//     ) {
+//       this.FetchImg();
+//     }
+//     // if (prevState.pagination !== pagination && search !== '') {
+//     //   this.setState({ galleryColection: [], page: 1 });
+//     //   this.FetchImg();
+//     // }
+//   }
+
+//   FetchImg = async () => {
+//     const { page, pagination } = this.state;
+//     if (this.state.search === '') {
+//       return;
+//     }
+//     this.setState({ isLoading: true });
+//     try {
+//       const data = await fetchImagesWithQuery(
+//         this.state.search,
+//         this.state.page
+//       );
+//       const { hits, totalHits } = data;
+//       if (hits.length === 0) {
+//         toast.warn(
+//           '🦄 Sorry, there are no images matching your search query. Please try again.',
+//           {
+//             position: 'top-right',
+//             autoClose: 5000,
+//             hideProgressBar: false,
+//             closeOnClick: true,
+//             pauseOnHover: true,
+//             draggable: true,
+//             progress: undefined,
+//           }
+//         );
+//         return;
+//       } else if (page === 1 || pagination === 'Pagination') {
+//         this.setState({
+//           galleryColection: [...hits],
+//           totalHits,
+//         });
+//       } else if (pagination === 'LoadMore') {
+//         this.setState(state => ({
+//           galleryColection: [...state.galleryColection, ...hits],
+//           totalHits,
+//         }));
+//       }
+//     } catch (error) {
+//       this.setState({
+//         error,
+//       });
+//       toast.error('Sorry, something going wrong :(', {
+//         position: 'top-right',
+//         autoClose: 5000,
+//         hideProgressBar: false,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//         progress: undefined,
+//       });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+
+//   toggleModal = () => {
+//     // this.setState(({ showModal }) => ({
+//     //   showModal: !showModal,
+//     // }));
+//     this.setState(({ activeGalleryItem }) => ({
+//       activeGalleryItem: null,
+//     }));
+//   };
+//   radioBtnChange = value => {
+//     // this.setState(({ showModal }) => ({
+//     //   showModal: !showModal,
+//     // }));
+//     console.log(value);
+//     this.setState({ pagination: value, page: 1, galleryColection: [] });
+//   };
+
+//   onSelectGalleryItem = item => {
+//     this.setState({ activeGalleryItem: item });
+//   };
+
+//   heandleSubmitForm = ({ search }) => {
+//     // console.log(pagination);
+//     // if (pagination !== 'LoadMore') {
+//     //   this.setState({ pagination: true });
+//     // } else if (this.state.pagination) {
+//     //   this.setState({ pagination: false });
+//     // }
+//     const normalizedSearch = search.toLocaleLowerCase();
+//     if (normalizedSearch && normalizedSearch !== this.state.search) {
+//       this.setState({
+//         search: normalizedSearch,
+//         page: 1,
+//         galleryColection: [],
+//       });
+//     }
+//   };
+//   OnClickLoadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+//   OnPagination = page => {
+//     // console.log(page);
+//     this.setState({ page });
+//   };
+
+//   onDisableLoadMore = () => {
+//     const { totalHits, page } = this.state;
+//     if (!totalHits) {
+//       return true;
+//     } else if (totalHits - PER_PAGE * page < 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   };
+
+//   showBtnLoadMore = () => {
+//     const { totalHits } = this.state;
+//     if (totalHits / PER_PAGE > 1) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   };
+//   showGallery = () => {
+//     const { totalHits } = this.state;
+//     if (totalHits) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   };
+
+//   render() {
+//     const { activeGalleryItem, isLoading, totalHits, pagination } = this.state;
+//     // console.log(this.state.gellaryColection);
+//     const showBtnLoadMore = this.showBtnLoadMore();
+//     const showGallery = this.showGallery();
+//     const countPages = Math.ceil(totalHits / PER_PAGE);
+//     return (
+//       <AppWrapper>
+//         <SearchBar
+//           onSubmit={this.heandleSubmitForm}
+//           paginationMode={pagination}
+//           isSubmitting={isLoading}
+//           radioBtnChange={this.radioBtnChange}
+//         />
+//         {/* {isLoading && galleryColection.length === 0 && <GallaryContentLoader />} */}
+//         {isLoading && <Loader />}
+//         {showGallery && (
+//           <ImageGallery
+//             galleryColection={this.state.galleryColection}
+//             onSelectGalleryItem={item => {
+//               this.onSelectGalleryItem(item);
+//             }}
+//           />
+//         )}
+
+//         {activeGalleryItem && (
+//           <Modal
+//             onClose={this.toggleModal}
+//             activeGalleryItem={this.state.activeGalleryItem}
+//           />
+//         )}
+
+//         {showBtnLoadMore && pagination === 'LoadMore' && (
+//           <Button
+//             onLoadMore={this.OnClickLoadMore}
+//             onDisableLoadMore={this.onDisableLoadMore}
+//           />
+//         )}
+//         {pagination === 'Pagination' && totalHits / PER_PAGE > 1 && (
+//           <GalleryPagination
+//             onPagination={this.OnPagination}
+//             countPages={countPages}
+//             currentPage={this.state.page}
+//           />
+//         )}
+
+//         <ToastContainer
+//           position="top-right"
+//           autoClose={5000}
+//           hideProgressBar={false}
+//           newestOnTop={false}
+//           closeOnClick
+//           rtl={false}
+//           pauseOnFocusLoss
+//           draggable
+//           pauseOnHover
+//         />
+//       </AppWrapper>
+//     );
+//   }
+// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,48 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { ModalImg, Overlay, ModalContent } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-    // console.log('modal Mount');
-  }
+const Modal = ({ activeGalleryItem, onClose }) => {
+  // componentDidMount() {
+  //   window.addEventListener('keydown', this.handleKeyDown);
+  //   // console.log('modal Mount');
+  // }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-    // console.log('modal Unmount');
-  }
+  // componentWillUnmount() {
 
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  handleOverlayClick = event => {
+  //   // console.log('modal Unmount');
+  //   window.removeEventListener('keydown', this.handleKeyDown);
+  // }
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleOverlayClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    const { largeImageURL, tags } = this.props.activeGalleryItem;
-    // console.log(this.props.activeGalleryItem);
-    return createPortal(
-      <Overlay className="overlay" onClick={this.handleOverlayClick}>
-        <ModalContent className="modal">
-          <ModalImg src={largeImageURL} alt={tags} />
-        </ModalContent>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+
+  const { largeImageURL, tags } = activeGalleryItem;
+  // console.log(this.props.activeGalleryItem);
+  return createPortal(
+    <Overlay className="overlay" onClick={handleOverlayClick}>
+      <ModalContent className="modal">
+        <ModalImg src={largeImageURL} alt={tags} />
+      </ModalContent>
+    </Overlay>,
+    modalRoot
+  );
+};
 
 export default Modal;
 Modal.propTypes = {
@@ -48,3 +52,47 @@ Modal.propTypes = {
   }).isRequired,
   onClose: PropTypes.func.isRequired,
 };
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//     // console.log('modal Mount');
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//     // console.log('modal Unmount');
+//   }
+
+//   handleKeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   handleOverlayClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     const { largeImageURL, tags } = this.props.activeGalleryItem;
+//     // console.log(this.props.activeGalleryItem);
+//     return createPortal(
+//       <Overlay className="overlay" onClick={this.handleOverlayClick}>
+//         <ModalContent className="modal">
+//           <ModalImg src={largeImageURL} alt={tags} />
+//         </ModalContent>
+//       </Overlay>,
+//       modalRoot
+//     );
+//   }
+// }
+
+// export default Modal;
+// Modal.propTypes = {
+//   activeGalleryItem: PropTypes.shape({
+//     largeImageURL: PropTypes.string.isRequired,
+//     tags: PropTypes.string.isRequired,
+//   }).isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
